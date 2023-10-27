df4564133fc1a106eaebe29d78050bd13bad899d
josielee1212 <josie.digitalad@gmail.com>
2023-10-27 17:26:01 +0800

    style: add npm Standard tool for coding stlye maintaining

--- app.js
+++ app.js
@@ -4,36 +4,37 @@ const app = express()
 const port = 3000
 const restaurants = require('./public/jsons/restaurants.json').results
 
-app.engine('.hbs', engine({extname: '.hbs'}))
+app.engine('.hbs', engine({ extname: '.hbs' }))
 app.set('view engine', '.hbs')
 app.set('views', './views')
 app.use(express.static('public'))
-
 
 app.get('/', (req, res) => {
   res.redirect('/restaurants')
 })
 
 app.get('/restaurants', (req, res) => {
-  res.render('index', { restaurants: restaurants})
+  res.render('index', { restaurants })
 })
 
 app.get('/search', (req, res) => {
-const keyword = req.query.kw;
-const trimKeyword = keyword.replace(/\s/g, '').toLowerCase(); // 去除所有空格和變小寫
-  const matchedRestaurants = keyword ? restaurants.filter((store) => {
-    const name = store.name.toLowerCase().trim();
-    const category = store.category.toLowerCase().trim();
-    return name.includes(trimKeyword) || category.includes(trimKeyword);
-  }) : restaurants;
-  
-  res.render('index', { restaurants: matchedRestaurants, keyword });
-});
+  const keyword = req.query.kw
+  const trimKeyword = keyword.replace(/\s/g, '').toLowerCase() // 去除所有空格和變小寫
+  const matchedRestaurants = keyword
+    ? restaurants.filter((store) => {
+      const name = store.name.toLowerCase().trim()
+      const category = store.category.toLowerCase().trim()
+      return name.includes(trimKeyword) || category.includes(trimKeyword)
+    })
+    : restaurants
+
+  res.render('index', { restaurants: matchedRestaurants, keyword })
+})
 
 app.get('/restaurant/:id', (req, res) => {
   const id = req.params.id
   const restaurant = restaurants.find((store) => store.id.toString() === id)
-  res.render('show-page',{restaurant})
+  res.render('show-page', { restaurant })
 })
 
 app.listen(port, () => {
